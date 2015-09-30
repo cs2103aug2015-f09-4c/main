4575650514cce24ca243d10f03ba8ab188157bcf
YongJieYongJie <KhooYongJie@gmx.com>
2015-09-30 12:55:12 +0800

    Refactored Parser: tided ADD related methods; Added to Parser: method stubs for DELETE commands

--- Parser/Parser.cpp
+++ Parser/Parser.cpp
@@ -11,14 +11,19 @@ CommandTokens Parser::parse(std::string userInput) {
 
 	switch (primaryCommand) {
 	case Add:
-		extractAddCommand(userInput);/*
+		extractAddCommand(userInput);
+		break;
 	case Delete:
 		extractDeleteCommand(userInput);
+		break;
 	case Edit:
 		extractEditCommand(userInput);
+		break;
 	case Display:
 		extractDisplayCommand(userInput);
-	case Invalid:*/
+		break;
+	case Invalid:
+		return _commandTokens;
 	}
 
 	return _commandTokens;
@@ -26,54 +31,95 @@ CommandTokens Parser::parse(std::string userInput) {
 
 PrimaryCommandType Parser::extractPrimaryCommand(std::string userInput) {
 	PrimaryCommandType commandType = Invalid;
-	if (std::regex_match(userInput,
-		std::regex("[A][D][D] .*", std::regex_constants::ECMAScript | std::regex_constants::icase ))) {
+	if (isAddCommand(userInput)) {
 		commandType = Add;
 
-	}/* else if (std::regex_match(userInput, std::regex("[Dd][Ee][Ll][Ee][Tt][Ee] .*"))) {
+	} else if (isDeleteCommand(userInput)) {
 		commandType = Delete;
 
-	} else if (std::regex_match(userInput, std::regex("[Ee][Dd][Ii][Tt] .*"))) {
+	} else if (isEditCommand(userInput)) {
 		commandType = Edit;
 
-	} else if (std::regex_match(userInput, std::regex("[Dd][Ii][Ss][Pp][Ll][Aa][Yy] .*"))) {
+	} else if (isDisplayCommand(userInput)) {
 		commandType = Display;
-	}*/
+	}
 	return commandType;
 }
 
 void Parser::extractAddCommand(std::string userInput) {
-	if (hasStartAndEndDate(userInput)) {
-		extractAddCommandWithStartAndEndDate(userInput);
-	} else if (hasEndDate(userInput)) {
-		extractAddCommandWithEndDate(userInput);
+	if (isActivityTask(userInput)) {
+		extractActivityTask(userInput);
+	} else if (isTodoTask(userInput)) {
+		extractTodoTask(userInput);
 	} else {
-		extractAddCommandFloating(userInput);
+		extractFloatingTask(userInput);
 	}
 }
 
-bool Parser::hasStartAndEndDate(std::string userInput) {
+void Parser::extractDeleteCommand(std::string userInput) {
+	if (isDeleteAll(userInput)) {
+
+	} else if (isDeleteFromTo(userInput)) {
+
+	} else if (isDeleteBy(userInput)) {
+
+	} else if (isDeleteFloating(userInput)) {
+
+	} else if (isDeleteIndex(userInput)) {
+
+	}
+}
+
+// TODO: implement
+void Parser::extractEditCommand(std::string userInput) {
+}
+
+// TODO: implement
+void Parser::extractDisplayCommand(std::string userInput) {
+}
+
+bool Parser::isAddCommand(std::string& userInput) {
+	return std::regex_match(userInput,
+	                        std::regex("[A][D][D] .*", std::regex_constants::ECMAScript | std::regex_constants::icase));
+}
+
+bool Parser::isDeleteCommand(std::string& userInput) {
+	return std::regex_match(userInput, std::regex("[Dd][Ee][Ll][Ee][Tt][Ee] .*"));
+}
+
+bool Parser::isEditCommand(std::string& userInput) {
+	return std::regex_match(userInput, std::regex("[Ee][Dd][Ii][Tt] .*"));
+}
+
+bool Parser::isDisplayCommand(std::string& userInput) {
+	return std::regex_match(userInput, std::regex("[Dd][Ii][Ss][Pp][Ll][Aa][Yy] .*"));
+}
+
+// returns true if userInput contains "from" and subsequently "to";
+// case-insensitive
+bool Parser::isActivityTask(std::string userInput) {
 	return std::regex_match(userInput,
 		std::regex(".* [F][R][O][M] .*[T][O] .*",
 		std::regex_constants::ECMAScript | std::regex_constants::icase ));
 }
 
-bool Parser::hasEndDate(std::string userInput) {
+// returns true if userInput contains "by"; case-insensitive
+bool Parser::isTodoTask(std::string userInput) {
 	return std::regex_match(userInput,
 		std::regex(".* [B][Y] .*",
 		std::regex_constants::ECMAScript | std::regex_constants::icase ));
 }
 
-void Parser::extractAddCommandWithStartAndEndDate(std::string userInput) {
+// extract taskName, startDateTime, and endDateTime; and call the setters on
+// _commandTokens to set these three fields
+void Parser::extractActivityTask(std::string userInput) {
+	_commandTokens.setSecondaryCommand(SecondaryCommandType::Timed);
+
 	std::smatch matchResults;
 	std::regex_match(userInput, matchResults,
 		std::regex("[A][D][D] (.*) [F][R][O][M] (.*) [T][O] (.*)",
 		std::regex_constants::ECMAScript | std::regex_constants::icase ));
 
-	_commandTokens.setSecondaryCommand(SecondaryCommandType::Timed);
-
-	// TODO: decide whether to use taskName or details
-	_commandTokens.setTaskName(matchResults[1]);
 	std::vector< std::string > newDetails;
 	newDetails.push_back(matchResults[1]);
 	_commandTokens.setDetails(newDetails);
@@ -82,15 +128,16 @@ void Parser::extractAddCommandWithStartAndEndDate(std::string userInput) {
 	_commandTokens.setEndDateTime(parseDate(matchResults[3]));
 }
 
-void Parser::extractAddCommandWithEndDate(std::string userInput) {
+// extract taskName, and endDateTime; and call the setters on _commandTokens
+// to set these two fields
+void Parser::extractTodoTask(std::string userInput) {
+	_commandTokens.setSecondaryCommand(SecondaryCommandType::Todo);
+
 	std::smatch matchResults;
 	std::regex_match(userInput, matchResults,
 		std::regex("[A][D][D] (.*) [B][Y] (.*)",
 		std::regex_constants::ECMAScript | std::regex_constants::icase ));
 
-	_commandTokens.setSecondaryCommand(SecondaryCommandType::Todo);
-
-	_commandTokens.setTaskName(matchResults[1]);
 	std::vector< std::string > newDetails;
 	newDetails.push_back(matchResults[1]);
 	_commandTokens.setDetails(newDetails);
@@ -98,22 +145,24 @@ void Parser::extractAddCommandWithEndDate(std::string userInput) {
 	_commandTokens.setEndDateTime(parseDate(matchResults[2]));
 }
 
-void Parser::extractAddCommandFloating(std::string userInput) {
+// extract taskName; and call the setter on _commandTokens to set this field
+void Parser::extractFloatingTask(std::string userInput) {
+	_commandTokens.setSecondaryCommand(SecondaryCommandType::Floating);
+
 	std::smatch matchResults;
 	std::regex_match(userInput, matchResults,
 		std::regex("[A][D][D] (.*)",
 		std::regex_constants::ECMAScript | std::regex_constants::icase ));
 
-	_commandTokens.setSecondaryCommand(SecondaryCommandType::Floating);
-
-	_commandTokens.setTaskName(matchResults[1]);
 	std::vector< std::string > newDetails;
 	newDetails.push_back(matchResults[1]);
 	_commandTokens.setDetails(newDetails);
 }
 
+// TODO: implement natural language date processing
+// convert date from string to ptime; currently only accepts standard date
+// string
 boost::posix_time::ptime Parser::parseDate(std::string dateString) {
-	// TODO implement properly
 	boost::posix_time::ptime ptimePlaceholder(boost::posix_time::time_from_string(dateString));
 	return ptimePlaceholder;
 }
--- Parser/Parser.h
+++ Parser/Parser.h
@@ -12,8 +12,11 @@ class Parser
 {
 public:
 	CommandTokens parse(std::string userInput);
-
-private:
+	bool isAddCommand(std::string& userInput);
+	bool isDeleteCommand(std::string& userInput);
+	bool isEditCommand(std::string& userInput);
+	bool isDisplayCommand(std::string& userInput);
+	private:
 	CommandTokens _commandTokens;
 
 	// constructor made private to prevent instantiation
@@ -22,13 +25,22 @@ private:
 	PrimaryCommandType extractPrimaryCommand(std::string userInput);
 
 	void extractAddCommand(std::string userInput);
+	void extractDeleteCommand(std::string userInput);
+	void extractEditCommand(std::string userInput);
+	void extractDisplayCommand(std::string userInput);
 
-	bool hasStartAndEndDate(std::string userInput);
-	bool hasEndDate(std::string userInput);
+	bool isActivityTask(std::string userInput);
+	bool isTodoTask(std::string userInput);
 
-	void extractAddCommandWithStartAndEndDate(std::string userInput);
-	void extractAddCommandWithEndDate(std::string userInput);
-	void extractAddCommandFloating(std::string userInput);
+	bool isDeleteAll(std::string userInput);
+	bool isDeleteFromTo(std::string userInput);
+	bool isDeleteBy(std::string userInput);
+	bool isDeleteFloating(std::string userInput);
+	bool isDeleteIndex(std::string userInput);
+
+	void extractActivityTask(std::string userInput);
+	void extractTodoTask(std::string userInput);
+	void extractFloatingTask(std::string userInput);
 
 	boost::posix_time::ptime parseDate(std::string dateString);
 };
