c85a501d93b7546622d168869b7cf3c4f3823730
YongJieYongJie <KhooYongJie@gmx.com>
2015-10-05 02:54:32 +0800

    Refactors DateParser for easy adding of new supported formats

--- Parser/DateParser.h
+++ Parser/DateParser.h
@@ -8,5 +8,8 @@ class DateParser {
 public:
 	DateParser(void);
 	boost::posix_time::ptime parse(std::string dateString);
-};
 
+private:
+	bool isDDMMYYYY_TTTT(std::string dateString);
+	std::string parseDDMMYYYY_TTTT(std::string dateString);
+};
