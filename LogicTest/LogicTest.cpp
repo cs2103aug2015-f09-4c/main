33381b3d01426477edd86644ea770a03582badc0
Lam Yet Sin <lamandy93@live.cn>
2015-10-04 13:25:35 +0800

    Create Unit Test Case For Add Float, Add Todo and Add Timed Command Creation

--- LogicTest/LogicTest.cpp
+++ LogicTest/LogicTest.cpp
@@ -5,13 +5,56 @@ using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace LogicTest
 {		
-	TEST_CLASS(UnitTest1)
+	TEST_CLASS(CommandCreatorTest)
 	{
 	public:
 		
-		TEST_METHOD(commandCreatorTest)
+		TEST_METHOD(TestCreateAddFloatCommand)
 		{
-			// TODO: Your test code here
+			CommandCreator creator;
+			std::vector<std::string> details;
+			details.push_back(std::string("do something"));
+			CommandTokens token1(Add,Floating,details);
+			AddCommand* command = creator.testProcessAddCommand(token1);
+			Task task("do something");
+			Assert::AreEqual(true, command->getPrimaryCommandType()==Add);
+			Assert::AreEqual(true, command->getSecondaryCommandType()==Floating);
+			Assert::AreEqual(false, command->isExecutedSuccessfully());
+			Assert::AreEqual(true, command->isValid());
+			Assert::AreEqual(true, command->getTask() == task);
+		}
+
+		TEST_METHOD(TestCreateAddTodoCommand)
+		{
+			CommandCreator creator;
+			std::vector<std::string> details;
+			details.push_back(std::string("do something"));
+			std::string timeString("2002-01-20 23:59:59.000");
+			CommandTokens token1(Add,Todo,details, boost::posix_time::time_from_string(timeString));
+			AddCommand* command = creator.testProcessAddCommand(token1);
+			Task task("do something",boost::posix_time::time_from_string(timeString));
+			Assert::AreEqual(true, command->getPrimaryCommandType()==Add);
+			Assert::AreEqual(true, command->getSecondaryCommandType()==Todo);
+			Assert::AreEqual(false, command->isExecutedSuccessfully());
+			Assert::AreEqual(true, command->isValid());
+			Assert::AreEqual(true, command->getTask() == task);
+		}
+
+		TEST_METHOD(TestCreateAddTimedCommand)
+		{
+			CommandCreator creator;
+			std::vector<std::string> details;
+			details.push_back(std::string("do something"));
+			std::string timeString1("2002-01-20 23:59:59.000");
+			std::string timeString2("2002-01-21 23:59:59.000");
+			CommandTokens token1(Add,Timed,details, boost::posix_time::time_from_string(timeString1), boost::posix_time::time_from_string(timeString2));
+			AddCommand* command = creator.testProcessAddCommand(token1);
+			Task task("do something",boost::posix_time::time_from_string(timeString1),boost::posix_time::time_from_string(timeString2));
+			Assert::AreEqual(true, command->getPrimaryCommandType()==Add);
+			Assert::AreEqual(true, command->getSecondaryCommandType()==Timed);
+			Assert::AreEqual(false, command->isExecutedSuccessfully());
+			Assert::AreEqual(true, command->isValid());
+			Assert::AreEqual(true, command->getTask() == task);
 		}
 
 	};
