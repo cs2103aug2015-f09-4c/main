7b663aab44aafaf6c17f6d9dcc647e479487701b
YongJieYongJie <KhooYongJie@gmx.com>
2015-10-14 01:25:18 +0800

    Creates method stubs for test methods to be implemented; extracts into method code for building the expected output

--- ParserTest/ParserTest.cpp
+++ ParserTest/ParserTest.cpp
@@ -5,66 +5,55 @@ using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace ParserTest {
 	TEST_CLASS(ParserTester) {
-	public:
-		CommandTokens _expected, _actual;
+		public:
 		Parser _parser;
-		std::string _testUserInput;
 
 		TEST_METHOD(testTokeniseAddActivityCommand) {
-			_testUserInput = "add activityTask FROM 2002-01-20 23:59:59.000 to 2002-01-22 23:59:59.000";
-			_actual = _parser.parse(_testUserInput);
+			std::string testUserInput = "ADD activityTask FROM 2002-01-20 23:59:59.000 TO 2002-01-22 23:59:59.000";
+			CommandTokens actual, expected;
 
-			_expected.setPrimaryCommand(PrimaryCommandType::Add);
-			_expected.setSecondaryCommand(SecondaryCommandType::Timed);
-			_expected.setTaskName("activityTask");
+			actual = _parser.parse(testUserInput);
+			expected = buildExpectedCommandTokens(PrimaryCommandType::Add,
+			                                      SecondaryCommandType::Timed,
+			                                      "activityTask",
+			                                      "2002-01-20 23:59:59.000",
+			                                      "2002-01-22 23:59:59.000",
+			                                      -1);
 
-			std::vector< std::string > expectedDetails;
-			expectedDetails.push_back("activityTask");
-			_expected.setDetails(expectedDetails);
-
-			boost::posix_time::ptime expectedStartDate(boost::posix_time::time_from_string("2002-01-20 23:59:59.000"));
-			boost::posix_time::ptime expectedEndDate(boost::posix_time::time_from_string("2002-01-22 23:59:59.000"));
-			_expected.setStartDateTime(expectedStartDate);
-			_expected.setEndDateTime(expectedEndDate);
-
-			compareCommandTokens(_expected, _actual);
+			compareCommandTokens(expected, actual);
 		}
 
 		TEST_METHOD(testTokeniseAddTodoCommand) {
-			_testUserInput = "add todoTask by 2002-01-22 23:59:59.000";
-			_actual = _parser.parse(_testUserInput);
+			std::string testUserInput = "ADD todoTask BY 2002-01-22 23:59:59.000";
+			CommandTokens actual, expected;
 
-			_expected.setPrimaryCommand(PrimaryCommandType::Add);
-			_expected.setSecondaryCommand(SecondaryCommandType::Todo);
-			_expected.setTaskName("todoTask");
+			actual = _parser.parse(testUserInput);
+			expected = buildExpectedCommandTokens(PrimaryCommandType::Add,
+			                                      SecondaryCommandType::Todo,
+			                                      "todoTask",
+			                                      "",
+			                                      "2002-01-22 23:59:59.000",
+			                                      -1);
 
-			std::vector< std::string > expectedDetails;
-			expectedDetails.push_back("todoTask");
-			_expected.setDetails(expectedDetails);
-
-			boost::posix_time::ptime expectedEndDate(boost::posix_time::time_from_string("2002-01-22 23:59:59.000"));
-			_expected.setEndDateTime(expectedEndDate);
-
-			compareCommandTokens(_expected, _actual);
+			compareCommandTokens(expected, actual);
 		}
 
 		TEST_METHOD(testTokeniseAddFloatingCommand) {
-			_testUserInput = "add floatingTask";
-			_actual = _parser.parse(_testUserInput);
+			std::string testUserInput = "ADD floatingTask";
+			CommandTokens actual, expected;
+			actual = _parser.parse(testUserInput);
+			expected = buildExpectedCommandTokens(PrimaryCommandType::Add,
+			                                      SecondaryCommandType::Floating,
+			                                      "floatingTask",
+			                                      "",
+			                                      "",
+			                                      -1);
 
-			_expected.setPrimaryCommand(PrimaryCommandType::Add);
-			_expected.setSecondaryCommand(SecondaryCommandType::Floating);
-			_expected.setTaskName("floatingTask");
-
-			std::vector< std::string > expectedDetails;
-			expectedDetails.push_back("floatingTask");
-			_expected.setDetails(expectedDetails);
-
-			compareCommandTokens(_expected, _actual);
+			compareCommandTokens(expected, actual);
 		}
 
-		TEST_METHOD(testTokeniseDelete) {
-			std::string testUserInput = "delete from 2002-01-20 23:59:59.000 to 2002-01-22 23:59:59.000";
+		TEST_METHOD(testTokeniseDeleteFromTo) {
+			std::string testUserInput = "DELETE FROM 2002-01-20 23:59:59.000 TO 2002-01-22 23:59:59.000";
 			Parser parser;
 
 			PrimaryCommandType expectedCommand = Delete;
@@ -79,27 +68,97 @@ namespace ParserTest {
 			Assert::IsTrue(boost::posix_time::to_simple_string(expectedEndDate) == boost::posix_time::to_simple_string(output.getEndDateTime()));
 		}
 
-		TEST_METHOD(testTokeniseDisplay) {
+		TEST_METHOD(testTokeniseDeleteFrom) {
+		}
 
+		TEST_METHOD(testTokeniseDeleteAll) {
+		}
+
+		TEST_METHOD(testTokeniseDeleteBy) {
+		}
+
+		TEST_METHOD(testTokeniseDeleteIndex) {
+		}
+
+		TEST_METHOD(testTokeniseDisplayBy) {
+		}
+
+		TEST_METHOD(testTokeniseDisplayFrom) {
+		}
+
+		TEST_METHOD(testTokeniseDisplayAll) {
+		}
+
+		TEST_METHOD(testTokeniseDisplayFloating) {
+		}
+
+		TEST_METHOD(testTokeniseEditNameCommand) {
+		}
+
+		TEST_METHOD(testTokeniseEditStartDateCommand) {
+		}
+
+		TEST_METHOD(testTokeniseEditEndDateCommand) {
+		}
+
+		TEST_METHOD(testTokeniseCompleteCommand) {
+		}
+
+		TEST_METHOD(testDateParser) {
+		}
+
+		CommandTokens buildExpectedCommandTokens(PrimaryCommandType primaryCommandType,
+		                                         SecondaryCommandType secondaryCommandType,
+		                                         std::string taskName,
+		                                         std::string startDate,
+		                                         std::string endDate,
+		                                         int index) {
+
+			CommandTokens expected;
+
+			expected.setPrimaryCommand(primaryCommandType);
+			expected.setSecondaryCommand(secondaryCommandType);
+			expected.setTaskName(taskName);
+			expected.setIndex(index);
+
+			// if an empty string is passed in, the command has no startDate
+			if (startDate == "") {
+				boost::posix_time::ptime expectedStartDate; // default constructor gives a not_a_date_time object
+				expected.setStartDateTime(expectedStartDate);
+			} else {
+				boost::posix_time::ptime expectedStartDate(boost::posix_time::time_from_string(startDate));
+				expected.setStartDateTime(expectedStartDate);
+			}
+
+			if (endDate == "") {
+				boost::posix_time::ptime expectedEndDate; // default constructor gives a not_a_date_time object
+				expected.setEndDateTime(expectedEndDate);
+			} else {
+				boost::posix_time::ptime expectedEndDate(boost::posix_time::time_from_string(endDate));
+				expected.setEndDateTime(expectedEndDate);
+			}
+
+			return expected;
 		}
 
 		void compareCommandTokens(CommandTokens expected, CommandTokens actual) {
-			Assert::AreEqual(int (expected.getPrimaryCommand()), int (actual.getPrimaryCommand()));
-			Assert::AreEqual(int (expected.getSecondaryCommand()), int (actual.getSecondaryCommand()));
+
+			Assert::AreEqual(int(expected.getPrimaryCommand()), int(actual.getPrimaryCommand()));
+			Assert::AreEqual(int(expected.getSecondaryCommand()), int(actual.getSecondaryCommand()));
 			Assert::AreEqual(expected.getTaskName(), actual.getTaskName());
 			Assert::IsTrue(expected.getStartDateTime() == actual.getStartDateTime());
 			Assert::IsTrue(expected.getEndDateTime() == actual.getEndDateTime());
 			Assert::AreEqual(expected.getIndex(), actual.getIndex());
 
-			std::vector< std::string > expectedDetails, actualDetails;
-			expectedDetails = expected.getDetails();
-			actualDetails = actual.getDetails();
+			std::vector<std::string> expectedTags, actualTags;
+			expectedTags = expected.getTags();
+			actualTags = actual.getTags();
 
-			Assert::AreEqual(expectedDetails.empty(), actualDetails.empty());
-
-			if (!expectedDetails.empty()) {
-				for (unsigned i = 0; i < expectedDetails.size(); i++) {
-					Assert::AreEqual(expectedDetails.at(i), actualDetails.at(i));
+			// checks if tags are both empty, else iterates through each element and assert equality
+			Assert::AreEqual(expectedTags.empty(), actualTags.empty());
+			if (!expectedTags.empty()) {
+				for (unsigned i = 0; i < expectedTags.size(); i++) {
+					Assert::AreEqual(expectedTags.at(i), actualTags.at(i));
 				}
 			}
 		}
