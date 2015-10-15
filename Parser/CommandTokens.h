be487856265d33fd6e3d1a2697d903c789484cd7
Yong Jie <KhooYongJie@gmx.com>
2015-10-15 15:45:18 +0800

    Adds complete constructor with default senitnel arguments; also tidies alignment

--- Parser/CommandTokens.h
+++ Parser/CommandTokens.h
@@ -1,6 +1,7 @@
 #pragma once
+#include <exception>
 #include <vector>
-#include "../boost/date_time/posix_time/posix_time.hpp"
+#include "boost\date_time\posix_time\posix_time.hpp"
 
 // Command Type for main groups of operation
 enum PrimaryCommandType {
@@ -32,14 +33,31 @@ public:
 	//default constructor, used for invalid command
 	CommandTokens();
 
+	CommandTokens::CommandTokens(PrimaryCommandType primaryCommandType = Invalid,
+	                             SecondaryCommandType secondaryCommandType = None,
+	                             std::string taskName = "",
+	                             boost::posix_time::ptime startDateTime = boost::posix_time::ptime(),
+	                             boost::posix_time::ptime endDateTime = boost::posix_time::ptime(),
+	                             std::vector<std::string> tags = std::vector<std::string>(0),
+	                             int index = -1);
+
 	//Constructor for operation without date and time
-	CommandTokens(PrimaryCommandType command1, SecondaryCommandType command2, std::vector<std::string> details);
+	CommandTokens(PrimaryCommandType command1,
+	              SecondaryCommandType command2,
+	              std::vector<std::string> details);
 
 	//Constructor for operation with either start or end dateTime
-	CommandTokens(PrimaryCommandType command1, SecondaryCommandType command2, std::vector<std::string> details, boost::posix_time::ptime dateTime);
+	CommandTokens(PrimaryCommandType command1,
+	              SecondaryCommandType command2,
+	              std::vector<std::string> details,
+	              boost::posix_time::ptime dateTime);
 
 	//Constructor for operation with both date and time
-	CommandTokens(PrimaryCommandType command1, SecondaryCommandType command2, std::vector<std::string> details, boost::posix_time::ptime startDateTime, boost::posix_time::ptime endDateTime);
+	CommandTokens(PrimaryCommandType command1,
+	              SecondaryCommandType command2,
+	              std::vector<std::string> details,
+	              boost::posix_time::ptime startDateTime,
+	              boost::posix_time::ptime endDateTime);
 
 	bool isValid();
 	void resetMemberVariables();
@@ -47,7 +65,6 @@ public:
 	PrimaryCommandType getPrimaryCommand();
 	SecondaryCommandType getSecondaryCommand();
 	std::string getTaskName();
-	std::vector<std::string>& getDetails();
 	std::vector<std::string>& getTags();
 	boost::posix_time::ptime getStartDateTime();
 	boost::posix_time::ptime getEndDateTime();
@@ -56,7 +73,6 @@ public:
 	void setPrimaryCommand(PrimaryCommandType newPrimaryCommand);
 	void setSecondaryCommand(SecondaryCommandType newSecondaryCommand);
 	void setTaskName(std::string newTaskName);
-	void setDetails(std::vector<std::string> newDetails);
 	void setTags(std::vector<std::string> newTags);
 	void setStartDateTime(boost::posix_time::ptime newStartDateTime);
 	void setEndDateTime(boost::posix_time::ptime newEndDateTime);
@@ -66,9 +82,14 @@ private:
 	PrimaryCommandType _primaryCommandType;
 	SecondaryCommandType _secondaryCommandType;
 	std::string _taskName;
-	std::vector<std::string> _details;
-	std::vector<std::string> _tags;
 	boost::posix_time::ptime _startDateTime;
 	boost::posix_time::ptime _endDateTime;
+	std::vector<std::string> _tags;
 	int _index;
+};
+
+class CommandDoesNotExistException: public std::exception {
+	virtual const char* what() const throw() override {
+		return "Command does not exist";
+	}
 };
