402c0e0a00359be717c49785ff241fd9c2e6ce38
Lam Yet Sin <lamandy93@live.cn>
2015-10-22 21:48:14 +0800

    Export is now supported

---
+++ Logic/Command_Export.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "Command.h"
+
+const std::string MESSAGE_EXPORT_INVALID_FILEPATH = "Invalid file path. Export cannot be done.";
+const std::string MESSAGE_EXPORT_SUCCESS = "Data have been exported to %s.";
+
+class ExportCommand : public Command {
+private:
+	std::string _filePath;
+public:
+	ExportCommand(std::string);
+	UIFeedback Command::execute(RunTimeStorage*);
+	UIFeedback Command::undo();
+	bool Command::canUndo();
+	~ExportCommand(void);
+};
+
