7d9a92e55aca15001bad51a0f88888986501652e
Yong Jie <KhooYongJie@gmx.com>
2015-10-22 22:27:37 +0800

    Fixes problem where dynamic allocation of memory changes flow of control

--- Parser/Parser.cpp
+++ Parser/Parser.cpp
@@ -1,15 +1,5 @@
 #include <assert.h>
 #include "Parser.h"
-#include "AddCommandTokeniser.h"
-#include "CompleteCommandTokeniser.h"
-#include "DeleteCommandTokeniser.h"
-#include "DisplayCommandTokeniser.h"
-#include "ExportCommandTokeniser.h"
-#include "EditCommandTokeniser.h"
-#include "RefreshCommandTokeniser.h"
-#include "TagCommandTokeniser.h"
-#include "UndoCommandTokeniser.h"
-#include "UntagCommandTokeniser.h"
 
 Parser::Parser(void) {
 	_logger = Logger::getInstance();
@@ -22,98 +12,44 @@ Parser::Parser(void) {
 CommandTokens Parser::parse(std::string userInput) {
 	_logger->logINFO("Parsing user input: " + userInput);
 
-	// inclusion guard
-	CommandTokens::PrimaryCommandType primaryCommandType = getPrimaryCommand(userInput);
-	if (primaryCommandType == CommandTokens::PrimaryCommandType::Invalid) {
+	try {
+		selectCommandTokeniser(userInput);
+	}
+	catch (CommandDoesNotExistException& e) {
+		_logger->logINFO(e.what());
 		return _invalidCommandTokens;
 	}
-
-	initialiseCommandTokeniser(primaryCommandType);
 
 	return _commandTokeniser->tokeniseUserInput(userInput);
 }
 
-CommandTokens::PrimaryCommandType Parser::getPrimaryCommand(std::string userInput) {
-	CommandTokens::PrimaryCommandType primaryCommandType;
-	try {
-		primaryCommandType = parsePrimaryCommand(userInput);
-	} catch (CommandDoesNotExistException& e) {
-		_logger->logINFO(e.what());
-		primaryCommandType = CommandTokens::PrimaryCommandType::Invalid;
-	}
-	return primaryCommandType;
-}
-
-void Parser::initialiseCommandTokeniser(CommandTokens::PrimaryCommandType primaryCommandType) {
-	// inclusion guard in calling function should have dealt with Invalid commands
-	assert(primaryCommandType != CommandTokens::PrimaryCommandType::Invalid);
-
-	switch (primaryCommandType) {
-		case CommandTokens::PrimaryCommandType::Add:
-			_commandTokeniser = new AddCommandTokeniser;
-			break;
-		case CommandTokens::PrimaryCommandType::Complete:
-			_commandTokeniser = new CompleteCommandTokeniser;
-			break;
-		case CommandTokens::PrimaryCommandType::Delete:
-			_commandTokeniser = new DeleteCommandTokeniser;
-			break;
-		case CommandTokens::PrimaryCommandType::Display:
-			_commandTokeniser = new DisplayCommandTokeniser;
-			break;
-		case CommandTokens::PrimaryCommandType::Edit:
-			_commandTokeniser = new EditCommandTokeniser;
-			break;
-		case CommandTokens::PrimaryCommandType::Export:
-			_commandTokeniser = new ExportCommandTokeniser;
-			break;
-		case CommandTokens::PrimaryCommandType::Refresh:
-			_commandTokeniser = new RefreshCommandTokeniser;
-			break;
-		case CommandTokens::PrimaryCommandType::Tag:
-			_commandTokeniser = new TagCommandTokeniser;
-			break;
-		case CommandTokens::PrimaryCommandType::Undo:
-			_commandTokeniser = new UndoCommandTokeniser;
-			break;
-		case CommandTokens::PrimaryCommandType::Untag:
-			_commandTokeniser = new UntagCommandTokeniser;
-			break;
-		default:
-			// default should not be reached because of inclusion guard in
-			// calling function and assertion in this function
-			return;
-	}
-}
-
-CommandTokens::PrimaryCommandType Parser::parsePrimaryCommand(std::string userInput) {
+void Parser::selectCommandTokeniser(std::string userInput) {
 	CommandTokens::PrimaryCommandType commandType;
 
 	if (isAddCommand(userInput)) {
-		commandType = CommandTokens::PrimaryCommandType::Add;
+		_commandTokeniser = &_addCommandTokeniser;
 	} else if (isCompleteCommand(userInput)) {
-		commandType = CommandTokens::PrimaryCommandType::Complete;
+		_commandTokeniser = &_completeCommandTokeniser;
 	} else if (isDeleteCommand(userInput)) {
-		commandType = CommandTokens::PrimaryCommandType::Delete;
+		_commandTokeniser = &_deleteCommandTokeniser;
 	} else if (isDisplayCommand(userInput)) {
-		commandType = CommandTokens::PrimaryCommandType::Display;
+		_commandTokeniser = &_displayCommandTokeniser;
 	} else if (isEditCommand(userInput)) {
-		commandType = CommandTokens::PrimaryCommandType::Edit;
+		_commandTokeniser = &_editCommandTokeniser;
 	} else if (isExportCommand(userInput)) {
-		commandType = CommandTokens::PrimaryCommandType::Export;
+		_commandTokeniser = &_exportCommandTokeniser;
 	} else if (isRefreshCommand(userInput)) {
-		commandType = CommandTokens::PrimaryCommandType::Refresh;
+		_commandTokeniser = &_refreshCommandTokeniser;
 	} else if (isTagCommand(userInput)) {
-		commandType = CommandTokens::PrimaryCommandType::Tag;
+		_commandTokeniser = &_tagCommandTokeniser;
 	} else if (isUndoCommand(userInput)) {
-		commandType = CommandTokens::PrimaryCommandType::Undo;
+		_commandTokeniser = &_undoCommandTokeniser;
 	} else if (isUntagCommand(userInput)) {
-		commandType = CommandTokens::PrimaryCommandType::Untag;
+		_commandTokeniser = &_untagCommandTokeniser;
 	} else {
+		_commandTokeniser = nullptr;
 		throw CommandDoesNotExistException();
 	}
-
-	return commandType;
 }
 
 bool Parser::isAddCommand(std::string& userInput) {
@@ -152,6 +88,6 @@ bool Parser::isUndoCommand(std::string& userInput) {
 	return UndoCommandTokeniser::isUndoCommand(userInput);
 }
 
-bool Parser::isUntagCommand(std::string & userInput) {
+bool Parser::isUntagCommand(std::string& userInput) {
 	return UntagCommandTokeniser::isUntagCommand(userInput);
 }
