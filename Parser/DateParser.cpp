9aef64ea279af8fa1d3038229f78eb90c3c65d11
YongJieYongJie <KhooYongJie@gmx.com>
2015-10-28 22:05:14 +0800

    Tidies regex in DateParser.cpp

--- Parser/DateParser.cpp
+++ Parser/DateParser.cpp
@@ -26,22 +26,22 @@ boost::posix_time::ptime DateParser::parse(std::string dateString) {
 
 bool DateParser::isDDMMYYYY_TTTT(std::string dateString) {
 	return std::regex_match(dateString,
-	                        std::regex("[0-9]{2}-[0-9]{2}-[0-9]{4} [0-9]{4}",
+	                        std::regex("[0-9]{2}-[0-9]{2}-[0-9]{4} [0-9]{2}[0-9]{2}",
 	                                   std::regex_constants::ECMAScript | std::regex_constants::icase));
 }
 
 std::string DateParser::parseDDMMYYYY_TTTT(std::string dateString) {
 	std::smatch matchResults;
 	std::regex_match(dateString, matchResults,
-	                 std::regex("([0-9]{2})-([0-9]{2})-([0-9]{4}) ([0-9]{4})",
+	                 std::regex("([0-9]{2})-([0-9]{2})-([0-9]{4}) ([0-9]{2})([0-9]{2})",
 	                            std::regex_constants::ECMAScript | std::regex_constants::icase));
 
 	std::string day, month, year, hour, minute, formattedDateString;
 	day = matchResults[1];
 	month = matchResults[2];
 	year = matchResults[3];
-	hour = matchResults[4].str().substr(0, 2);
-	minute = matchResults[4].str().substr(2, 2);
+	hour = matchResults[4];
+	minute = matchResults[5];
 	formattedDateString = year + "-" + month + "-" + day + " " + hour + ":" + minute + ":00.000";
 
 	return formattedDateString;
