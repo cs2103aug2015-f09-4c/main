ad92abfd0d60a43bf1d104aca8c5f8c442cec41e
Yong Jie <KhooYongJie@gmx.com>
2015-10-31 16:11:41 +0800

    Refactors CompleteCommandTokeniser class by renaming methods and removing comments

--- Parser/CommandTokenisers/CompleteCommandTokeniser.cpp
+++ Parser/CommandTokenisers/CompleteCommandTokeniser.cpp
@@ -9,31 +9,31 @@ CompleteCommandTokeniser::~CompleteCommandTokeniser(void) {
 	// nothing here
 }
 
-CommandTokens CompleteCommandTokeniser::tokeniseUserInput(std::string userInput) {
-	_commandTokens.reset();
-	_commandTokens.setPrimaryCommand(CommandTokens::PrimaryCommandType::Complete);
-
-	tokeniseCompleteIndexCommand(userInput);
-	return _commandTokens;
-}
-
 bool CompleteCommandTokeniser::isValidCommand(std::string userInput) {
-	return isCompleteCommand(userInput);
-}
-
-bool CompleteCommandTokeniser::isCompleteCommand(std::string userInput) {
-	if (isCompleteIndexCommand(userInput)) {
+	if (isCompleteIndex(userInput)) {
 		return true;
 	}
 	return false;
 }
 
-bool CompleteCommandTokeniser::isCompleteIndexCommand(std::string userInput) {
-	return std::regex_match(userInput, std::regex("complete [0-9]+",
-	                                              std::regex_constants::ECMAScript | std::regex_constants::icase));
+CommandTokens CompleteCommandTokeniser::tokeniseUserInput(std::string userInput) {
+	_commandTokens.reset();
+	_commandTokens.setPrimaryCommand(CommandTokens::PrimaryCommandType::Complete);
+
+	if (isCompleteIndex(userInput)) {
+		tokeniseCompleteIndex(userInput);
+	}
+
+	return _commandTokens;
 }
 
-void CompleteCommandTokeniser::tokeniseCompleteIndexCommand(std::string userInput) {
+bool CompleteCommandTokeniser::isCompleteIndex(std::string userInput) {
+	return std::regex_match(userInput,
+	                        std::regex("complete [0-9]+",
+	                                   std::regex_constants::ECMAScript | std::regex_constants::icase));
+}
+
+void CompleteCommandTokeniser::tokeniseCompleteIndex(std::string userInput) {
 	_commandTokens.setSecondaryCommand(CommandTokens::SecondaryCommandType::Index);
 
 	std::smatch matchResults;
--- Parser/CommandTokenisers/CompleteCommandTokeniser.h
+++ Parser/CommandTokenisers/CompleteCommandTokeniser.h
@@ -2,23 +2,16 @@
 #pragma once
 #include "..\CommandTokeniser.h"
 
-// serves two related purposes:
-// first, provides a static public method to check if an entered command is
-// indeed of Complete PrimaryCommandType
-// second, tokenises the entered command into the various arguments
 class CompleteCommandTokeniser : public CommandTokeniser {
 public:
 	CompleteCommandTokeniser(void);
 	virtual ~CompleteCommandTokeniser(void);
-	virtual CommandTokens tokeniseUserInput(std::string userInput) override;
-	static bool isCompleteCommand(std::string userInput);
+
 	virtual bool isValidCommand(std::string userInput) override;
+	virtual CommandTokens tokeniseUserInput(std::string userInput) override;
 
 private:
-	// identifiers to determine the exact type of COMPLETE command called,
-	// currently only one type
-	static bool isCompleteIndexCommand(std::string userInput);
+	static bool isCompleteIndex(std::string userInput);
 
-	// tokenisers for the various types of COMPLETE commands
-	void tokeniseCompleteIndexCommand(std::string userInput);
+	void tokeniseCompleteIndex(std::string userInput);
 };
