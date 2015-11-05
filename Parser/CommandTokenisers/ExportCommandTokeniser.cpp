a10f085a627fca8cd7fe5f4a5f2c36a5d0c780bc
YongJieYongJie <KhooYongJie@gmx.com>
2015-11-05 14:13:32 +0800

    Modifies regex to allow spaces when specifying filepaths

--- Parser/CommandTokenisers/ExportCommandTokeniser.cpp
+++ Parser/CommandTokenisers/ExportCommandTokeniser.cpp
@@ -29,13 +29,13 @@ CommandTokens ExportCommandTokeniser::tokeniseUserInput(std::string userInput) {
 }
 
 bool ExportCommandTokeniser::isExportToLocalDisk(std::string userInput) {
-	return isRegexMatch(&userInput,"EXPORT [^ ]+");
+	return isRegexMatch(&userInput,"EXPORT .+");
 }
 
 void ExportCommandTokeniser::tokeniseExportToLocalDisk(std::string userInput, CommandTokens* outputCommandTokens) {
 	outputCommandTokens->setSecondaryCommand(CommandTokens::SecondaryCommandType::None);
 
-	std::smatch matchResults = getRegexMatches(&userInput, "EXPORT ([^ ]+)");
+	std::smatch matchResults = getRegexMatches(&userInput, "EXPORT (.+)");
 
 	std::string exportPath = matchResults[1];
 	outputCommandTokens->setOtherCommandParameter(exportPath);
